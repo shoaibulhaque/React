e39f00006b782c7528f32751906bbd99ea7c39af
Shoaib Ul Haq <shoaibulhaqq@outlook.com>
2024-08-13 17:04:39 +0500

    1:00:4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');  // we cannot change value directly, so we have used state
   const showEmployees = true;
+  // let role = 'dev' 
   return (
     <div className="App">
       {showEmployees ? (
       <>
+        <input type='text' onChange={
+          (e) => {
+            console.log(e.target.value);
+            setRole(e.target.value)
+            // role = e.target.value; // this does not work !
+          }}
+          
+        />
           <Employee name = "Shoaib" role = "Intern"/>
-          <Employee name = "Saqib" /> 
+          <Employee name = "Saqib" role = {role} /> 
       </>
      ) : (
       <p> You can not see the Employees</p>
