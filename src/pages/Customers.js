a95a886d0bd7f16e87838a939272220d6153168d
Shoaib Ul Haq <shoaibulhaqq@outlook.com>
2024-08-21 22:55:34 +0500

    Consume the Backend API

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,9 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Customers() {
+  const [customers, setCustomers] = useState();
+
   useEffect(() => {
     console.log("Fetching...");
-  });
+    fetch("http://localhost:8000/api/customers/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setCustomers(data.customers);
+      });
+  }, []); // Added an empty dependency, so it does this once on inital load
 
-  return <h1>Hello there!</h1>;
+  return (
+    <>
+      <h1>Here are our Customers! </h1>
+      {customers
+        ? customers.map((customer) => {
+            return <p>{customer.name}</p>;
+          })
+        : null}
+    </>
+  );
 }
