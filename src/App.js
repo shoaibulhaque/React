dff89bc67079cff084ef605460cd44a40bfbd1fc
Shoaib Ul Haq <shoaibulhaqq@outlook.com>
2024-09-01 00:32:34 +0500

    Clean up code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./index.css";
 import Header from "./components/Header";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import Employees from "./pages/Employees";
 import Customers from "./pages/Customers";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -10,13 +10,42 @@ import NotFound from "./components/NotFound";
 import Customer from "./pages/Customer";
 import Login from "./pages/Login";
 import Footer from "./components/Footer";
+import { baseURL } from "./shared";
 
 export const loginContext = createContext(); // Created a context
 
 function App() {
   // check localStorage for an access token --> may be expired
   // const [loggedIn, setLoggedIn] = useState(localStorage.access ? true : false);
+
   // long term goal --> use refresh token and if it works, stay logged in, otherwise, send to login page
+  useEffect(() => {
+    function refreshTokens() {
+      if (localStorage.refresh) {
+        const url = baseURL + "api/token/refresh/";
+        fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            refresh: localStorage.refresh,
+          }),
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            localStorage.access = access;
+            localStorage.refresh = refresh;
+            setLoggedIn(true);
+          });
+      }
+    }
+    const minutes = 1000 * 60;
+    refreshTokens();
+    setInterval(refreshTokens, minutes * 3);
+  }, []); // dependency array is left empty so that it executes only once
   const [loggedIn, setLoggedIn] = useState(localStorage.access ? true : false);
 
   function changeLoggedIn(value) {
